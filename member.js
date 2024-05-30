694536bbec89968c8678a330d91f95c2b5636473
PedroTH16 <139144348+PedroTH16@users.noreply.github.com>
2024-05-30 13:23:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  const skills = [
+    {
+      name: 'HTML',
+      level: 8,
+    },
+    {
+      name: 'CSS',
+      level: 7,
+    },
+    {
+      name: 'JavaScript',
+      level: 6,
+    },
+  ];
+
+  return skills;
+}
